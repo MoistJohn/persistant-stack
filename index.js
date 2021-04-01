e4a1d453787d256af2667df8e750e20e8e2736c0
Yonatan Korem <yonatan.korem@gmail.com>
2021-04-02 00:33:21 +0300

    controller created with RESTful routing

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const express = require('express');
+const express = require("express");
+const { Stack } = require("./stack");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const stack = new Stack();
+
+app.get("/", (req, res) => {
+  res.send(stack.pop());
+});
+
+app.get("/peek", (req, res) => {
+  res.send(stack.peek());
+});
+
+app.post("/:value", (req, res) => {
+  stack.push(req.params.value);
+  res.send(req.params.value);
+});
+
+app.put("/revert", (req, res) => {
+  stack.revert();
+  res.send('Stack was reverted');
+});
+
+app.put("/:value", (req, res) => {
+  stack.push(req.params.value);
+  res.send(req.params.value);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
