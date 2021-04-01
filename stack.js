de96646ca59c94e2d14e0f76a8dee874ad61fb84
Yonatan Korem <yonatan.korem@gmail.com>
2021-04-02 01:06:43 +0300

    introduced persistancy with lowdb. stack initializes from db file

--- stack.js
+++ stack.js
@@ -1,31 +1,47 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
 class Stack {
-    #size = 0;
-    #value = [];
-    #flipped = false;
-    constructor() {
-    }
+  #size = 0;
+  #value = [];
+  #flipped = false;
+  constructor() {
 
-    get isEmpty() {
-        return this.#size === 0;
-    }
+    // TODO refactor db operations to another class
+    const adapter = new FileSync("db.json");
+    const db = low(adapter);
 
-    push(str) {
-        this.#size++;
-        this.#flipped ? this.#value.unshift(str) : this.#value.push(str);
-    }
+    // Set some defaults (required if your JSON file is empty)
+    db.defaults({ stack: [], flipped: false, size: 0 }).write();
 
-    pop() {
-        if (this.isEmpty) { return null; }
-        this.#size--;
-        return this.#flipped ? this.#value.shift() : this.#value.pop();
-    }
+    this.#value = db.get('stack').value();
+    this.#flipped = db.get('flipped').value();
+    this.#size = db.get('size').value();
+  }
 
-    peek() {
-        return this.#value[this.#size - 1];
-    }
+  get isEmpty() {
+    return this.#size === 0;
+  }
 
-    revert() {
-        this.#flipped = !this.#flipped;
+  push(str) {
+    this.#size++;
+    this.#flipped ? this.#value.unshift(str) : this.#value.push(str);
+  }
+
+  pop() {
+    if (this.isEmpty) {
+      return null;
     }
+    this.#size--;
+    return this.#flipped ? this.#value.shift() : this.#value.pop();
+  }
+
+  peek() {
+    return this.#value[this.#size - 1];
+  }
+
+  revert() {
+    this.#flipped = !this.#flipped;
+  }
 }
-module.exports = { Stack }
+module.exports = { Stack };
